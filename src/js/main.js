ad17811fb99cee3646c12f69a9b07470c8544623
Duarte Carrilho da Graça <duarte.carrilho.freelance@theguardian.com>
2016-10-10 10:34:27 +0100

    explore middletown series page

--- src/js/main.js
+++ src/js/main.js
@@ -1,74 +1,43 @@
-import reqwest from 'reqwest'
+// import reqwest from 'reqwest'
 import mainHTML from './text/main.html!text'
-import share from './lib/share'
+// import share from './lib/share'
 // import template from './text/template.html!text'
 // import handlebars from 'handlebars'
 
-var shareFn = share('Interactive title', 'http://gu.com/p/URL', '#Interactive');
+// var shareFn = share('Interactive title', 'http://gu.com/p/URL', '#Interactive');
 var el;
 var data;
 
 export function init(dom, context, config, mediator) {
-    el = dom;
-    el.innerHTML = mainHTML.replace(/%assetPath%/g, config.assetPath);
+  el = dom;
+  el.innerHTML = mainHTML.replace(/%assetPath%/g, config.assetPath);
 
-    // var url = "https://interactive.guim.co.uk/docsdata-test/1lZsRxsU6rF4sAruy7MEIBbOlz3x3egAF9PE1wnbQPVU.json"
-    // reqwest({
-    //     url: url,
-    //     type: 'json',
-    //     crossOrigin: true,
-    //     success: function(resp){
-    //         data = resp;
-    //         templateInit(config)
-    //     }
-    // });
+
+  // play video on click
+  document.querySelector('.what__video .cover').onclick = function() {
+    document.querySelector('.what__video').classList.add('will-play');
+    var embed = document.querySelector('.what__video .embed');
+    embed.setAttribute('src', embed.getAttribute('data-src'));
+  }
+
+  var middletown = document.getElementById('g-explore-middletown');
+  if (getParameterByName('mode')) {
+    middletown.setAttribute('data-component', 'middletown series page : ' + getParameterByName('mode'));
+  } else {
+    middletown.setAttribute('data-component', 'middletown series page : separate page');
+  }
+
+
+
 }
 
-function templateInit(config){
-    // console.log(data)
-    // // Header
-    // var introText = data.intro_quote.split(' ');
-    // var introTextLength = introText.length;
-    // var newIntroText = "";
-    //
-    // introText.forEach(function(word,i){
-    //     newIntroText += word + " ";
-    //     if(i === introTextLength - 10){
-    //         newIntroText += '<img src="' + config.assetPath + '/assets/imgs/Ewen.png" class="img-ewen"/>'
-    //     }
-    // })
-    //
-    // el.querySelector('#header-standfirst span').innerHTML = newIntroText;
-    //
-    // removeHeaderContent();
-    //
-    // // Rest of page
-    // var mainTemplate = Handlebars.compile(template);
-    // var mainTemplateParsed = mainTemplate(data).replace(/%assetPath%/g,config.assetPath);
-    //
-    // el.querySelector('#interactive-content').innerHTML = mainTemplateParsed;
-    //
-    // var playContainers = document.querySelectorAll('.talking-head');
-    //
-    // for(var i =0;i <playContainers.length; i++){
-    //     playContainers[i].addEventListener('click',function(e){
-    //         var isActive = e.target.parentElement.className.indexOf('video-active') > -1 ? true : false;
-    //         for(var j =0;j < playContainers.length; j++){
-    //             playContainers[j].className = " talking-head";
-    //             playContainers[j].querySelector('video').pause();
-    //         }
-    //         if(!isActive){
-    //             e.target.parentElement.className += " video-active";
-    //             e.target.parentElement.querySelector('video').play();
-    //             e.target.parentElement.querySelector('video').autoplay = true;
-    //         }
-    //     })
-    // }
-    //
-    // var placeHolder = document.querySelector('.video-placeholder');
-    //
-    // placeHolder.addEventListener('click',function(e){
-    //     el.querySelector('.interactive-video-container iframe').src = data.section_video.video_url + "#autoplay"
-    //     placeHolder.style.display = "none"
-    // })
+
+function getParameterByName(name) {
+    var url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
